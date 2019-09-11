ca0214a935839e521f71cf8ccf2fcd234303fd28
Martin <ryqiem@gmail.com>
2019-09-11 18:40:02 +0200

    Adding interface

---
+++ percentile-feedback/OS X Menu Bar/OS X Menu Bar 2017-08-09 20-42-54/OS X Menu Bar.app/Contents/Resources/data.js
@@ -0,0 +1,6 @@
+var midnight = "00:00";
+var midnight_seconds = 0;
+var today_wr = {date: new Date(2019, 7, 27), starts: [36102.0, 36154.0], stops: [36105.0, 36158.0]};
+var updateInterval;
+var refresh_delay = 43733.87873;
+var past_wrs = [];
